259501b3baf69711abd60f6afa04125dd564b406
lvjiawei333 <422214955@qq.com>
2021-12-19 00:04:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+cd package
+git clone https://github.com/kenzok8/openwrt-packages
+git clone https://github.com/kenzok8/small
