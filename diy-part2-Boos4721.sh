fba72aeefdd6cf3c3930fd88c172987380554d20
lvjiawei333 <422214955@qq.com>
2022-03-21 16:18:04 +0800

    Create diy-part2-Boos4721.sh

---
+++ diy-part2-Boos4721.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+Modify default IP
+sed -i 's/10.10.10.1/192.168.123.2/g' package/base-files/files/bin/config_generate
+#git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
+#git clone https://github.com/kenzok8/small package/small
+#git clone https://github.com/liuran001/openwrt-packages package/openwrt-packages
+#git clone https://hub.fastgit.org/OpenWrt-Actions/openwrt-package-lienol.git package/openwrt-package-lienol
