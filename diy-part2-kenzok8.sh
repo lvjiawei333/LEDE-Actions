8fd671fd4f49d11d16787976d0291e1db5a3cb14
lvjiawei333 <422214955@qq.com>
2022-04-06 15:04:11 +0800

    Update diy-part2-kenzok8.sh

--- diy-part2-kenzok8.sh
+++ diy-part2-kenzok8.sh
@@ -11,16 +11,16 @@
 #
 
 Modify default IP
-sed -i 's/192.168.15.1/192.168.123.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
 #git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
 #git clone https://github.com/kenzok8/small package/small
 #git clone https://github.com/liuran001/openwrt-packages package/openwrt-packages
 #git clone https://hub.fastgit.org/OpenWrt-Actions/openwrt-package-lienol.git package/openwrt-package-lienol
-#复制内核5.10版本CPU超频补丁
-#\cp -rf preset-models/322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
+复制内核5.10版本CPU超频补丁
+\cp -rf preset-models/322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
 #设置WIFI
 #sed -i 's/OpenWrt/coolxiaomi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #sed -i 's/wireless.default_radio${devidx}.encryption=none/wireless.default_radio${devidx}.encryption=psk-mixed/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #sed -i '/encryption/a\set wireless.default_radio${devidx}.key=coolxiaomi' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-#切换ramips内核到5.10
-#sed -i '/KERNEL_PATCHVER/cKERNEL_PATCHVER:=5.10' target/linux/ramips/Makefile
+切换ramips内核到5.10
+sed -i '/KERNEL_PATCHVER/cKERNEL_PATCHVER:=5.10' target/linux/ramips/Makefile
