ef71d15e243d2c6e5d8ef982b325c0200841e493
lvjiawei333 <422214955@qq.com>
2022-04-07 01:09:09 +0800

    Update diy-part2-kenzok8.sh

--- diy-part2-kenzok8.sh
+++ diy-part2-kenzok8.sh
@@ -10,20 +10,20 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-Modify default IP
+#Modify default IP
 sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
 #git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
 #git clone https://github.com/kenzok8/small package/small
 #git clone https://github.com/liuran001/openwrt-packages package/openwrt-packages
 #git clone https://hub.fastgit.org/OpenWrt-Actions/openwrt-package-lienol.git package/openwrt-package-lienol
-复制内核5.10版本CPU超频补丁
+#复制内核5.10版本CPU超频补丁
 cp -rf files/322-mt7621-fix-cpu-clk-add-clkdev.patch target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
-复制修改32MK2P（如果后续增加mt7621机型请注释掉第二行，重新修改）
-cp -rf files/mt7621_phicomm_k2p.dts /target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-cp -rf files/mt7621.mk /target/linux/ramips/image/mt7621.mk
+#复制修改32MK2P（如果后续增加mt7621机型请注释掉第二行，重新修改）
+#cp -rf files/mt7621_phicomm_k2p.dts /target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+#cp -rf files/mt7621.mk /target/linux/ramips/image/mt7621.mk
 #设置WIFI
 #sed -i 's/OpenWrt/coolxiaomi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #sed -i 's/wireless.default_radio${devidx}.encryption=none/wireless.default_radio${devidx}.encryption=psk-mixed/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #sed -i '/encryption/a\set wireless.default_radio${devidx}.key=coolxiaomi' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-切换ramips内核到5.10
+#切换ramips内核到5.10
 sed -i '/KERNEL_PATCHVER/cKERNEL_PATCHVER:=5.10' target/linux/ramips/Makefile
